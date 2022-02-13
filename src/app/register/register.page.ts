2a39b11b60c8f4f805e095e086a056e8cc93a17d
Michi <bogensbergermichael@gmail.com>
2022-02-13 16:21:31 +0100

    changes

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  private formData: FormGroup;
+  selected_option: string;
 
-  constructor() { }
+  private split1 = false;
+  private split2 = false;
+  private split3 = false;
+  private split4 = false;
+
+
+  constructor() {}
 
   ngOnInit() {
+    this.formData = new FormGroup({
+      firstname: new FormControl(),
+      lastname: new FormControl(),
+      password: new FormControl(),
+      email: new FormControl(),
+      height: new FormControl(),
+      weight: new FormControl(),
+      days: new FormControl(),
+      split: new FormControl(),
+      split1: new FormControl(),
+      split2: new FormControl(),
+      split3: new FormControl(),
+      split4: new FormControl()
+    });
+  }
+
+  onSelectChange() {
+    switch (this.selected_option) {
+      case '1':
+        this.split1 = true;
+        this.split2 = false;
+        this.split3 = false;
+        this.split4 = false;
+
+        this.formData.value.split2 = null;
+        this.formData.value.split3 = null;
+        this.formData.value.split4 = null;
+
+        break;
+
+      case '2':
+        this.split1 = true;
+        this.split2 = true;
+        this.split3 = false;
+        this.split4 = false;
+
+        this.formData.value.split3 = null;
+        this.formData.value.split4 = null;
+        break;
+
+      case '3':
+        this.split1 = true;
+        this.split2 = true;
+        this.split3 = true;
+        this.split4 = false;
+
+        this.formData.value.split4 = null;
+        break;
+
+      case '4':
+        this.split1 = true;
+        this.split2 = true;
+        this.split3 = true;
+        this.split4 = true;
+        break;
+    }
+  }
+
+
+  onSubmit() {
+    console.log(this.formData.value);
+
   }
 
 
